c34b8e811544313c362706a0f4c485259f2ebfc1
mariaam <59062315+mariam-ms@users.noreply.github.com>
2021-08-20 20:20:00 +0200

    Update A-B-C-.cpp

--- A-B-C-.cpp
+++ A-B-C-.cpp
@@ -1,18 +1,36 @@
+//to clear screen from commands
+system("cls")
 
-
-//operators +,-,/,%,*
-
-int main () {
-
+  
+  
+// arithamtic operators +,-,/,%,*
 int counter=0; //encrement counter
-  counter++;
-  cout<<counter;
+   counter++;
+   cout<<counter;
   
-  cout<<counter++; //past encrement operator
-  cout<<++counter //pre encrement operator
+   cout<<counter++; //past encrement operator
+   cout<<++counter //pre encrement operator
+     
+     cout<<6*2;
+     cout<<6/2;
+
+//relational operators <,>,==,!=,<=,>=
+
+int a=5 , b=5;
+
+cout<<(a==b); //gives 1 or 0 (true or false )results 
+cout<<(b<=a);
+
+//logical operators && , || ,!=
+cout<<(a==5 && b==5);
+
+
+
+
+
+
   
 
-}
 
 
 
