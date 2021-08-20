f8201ce4ecbee465f461baf28b1126a23c6c3017
mariaam <59062315+mariam-ms@users.noreply.github.com>
2021-08-20 04:25:30 +0200

    Create conditional-statement.cpp

---
+++ conditional-statement.cpp
@@ -0,0 +1,27 @@
+/* if/else statement
+
+if (condition)
+{ run what's inside this curly brackets if condition is true }
+else 
+{ and run this if it's false }
+*/
+
+//even and odd program
+
+int main() {
+
+int num;
+couy<<"please enter a number to examine if it's even or odd";
+cin>> num;
+
+if(num%2==0)
+{ cout<<"number "<<$num<<" is an even number"<<endl;
+}
+else
+{ cout<<"number "<<$num<<" is an odd number"<<endl;
+}
+
+cout<<"try one more time.."
+}
+
+/* nested if
