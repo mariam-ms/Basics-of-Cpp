b67b10b58652abd5472a82c87539266daec169b9
mariaam <59062315+mariam-ms@users.noreply.github.com>
2021-08-20 17:52:08 +0200

    Create A-B-C-.cpp

---
+++ A-B-C-.cpp
@@ -0,0 +1,32 @@
+
+
+//operators +,-,/,%,*
+
+int main () {
+
+int counter=0; //encrement counter
+  counter++;
+  cout<<counter;
+  
+  cout<<counter++; //past encrement operator
+  cout<<++counter //pre encrement operator
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
